f0a977b571ef98cef11d5763645f75705d50dd31
Abhishek Pal <paljiabhishek184@gmail.com>
2024-07-09 16:38:43 +0530

    updated

--- src/Compments/Contact/Contact.jsx
+++ src/Compments/Contact/Contact.jsx
@@ -5,9 +5,9 @@ function Contact() {
     <div className='flex justify-center h-[79vh]'>
       <div >
         <h1 className='text-4xl font-medium mt-20'>Contact Me</h1>
-        <p className='text-xl'><b>Created by :</b>  <span>Deependra Rajput</span></p>
-        <p><b>Email Address :</b> <a href="">deependra186037@gmail.com</a></p>
-        <p><b>Github link :</b> <a href="https://github.com/Deep1416">github.com/Deep1416</a></p>
+        <p className='text-xl'><b>Created by :</b>  <span>Abhishek Pal</span></p>
+        <p><b>Email Address :</b> <a href="">paljiabhishek184@gmail.com</a></p>
+        <p><b>Github link :</b> <a href="https://github.com/abhirag9551236">github.com/abhirag955123</a></p>
       </div>
     </div>
   )
